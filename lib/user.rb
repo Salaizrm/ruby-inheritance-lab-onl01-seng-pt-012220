4ecbc0730cdee78609c83a43c4585546102c602f
Robert Salaiz <work.salaizrm@gmail.com>
2020-02-21 20:45:11 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -8,7 +8,7 @@ class User
   def intitialize (first_name, last_name)
     @first_name = first_name
     @last_name = last_name
-    @knowledge = []
+    @knowledge = Array.new
   end
   
   def first_name
