0c7b198fd449b750108b04013e4218f05fc3a89c
Robert Salaiz <work.salaizrm@gmail.com>
2020-02-21 20:56:00 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -4,9 +4,12 @@ class User
   
   attr_accessor :first_name, :last_name
   
+  @knowledge = []
+  
   def intitialize (first_name, last_name)
     @first_name = first_name
     @last_name = last_name
+    @knowledge << self
   end
   
   def first_name
